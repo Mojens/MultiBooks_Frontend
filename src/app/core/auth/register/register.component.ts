68a75d1bf8996b062f9f1814c00de943897c52f5
mohammadmurtada <moha135h@stud.kea.dk>
2023-11-25 20:35:17 +0100

    Register component Refactor og gør det lidt pænere

--- src/app/core/auth/register/register.component.ts
+++ src/app/core/auth/register/register.component.ts
@@ -14,29 +14,10 @@ import {TeamManagementApiService} from "../../../features/team-management/team-m
 })
 export class RegisterComponent implements OnInit {
 
-  userEmail: string = '';
-  password: string = '';
-  confirmPassword: string = '';
-  showSuccessMessage: string = '';
-  registerRequest: RegisterRequest = {
-    email: '',
+  formData = {
+    userEmail: '',
     password: '',
-    confirmPassword: ''
-  };
-
-
-  CVRNumber: number = 0;
-  VATNumber: string = '';
-  companyName: string = '';
-  address: string = '';
-  city: string = '';
-  zipCode: number = 0;
-  country: string = '';
-  phoneNumber: string = '';
-  website: string = '';
-  companyEmail: string = '';
-
-  businessTeamRequest: BusinessTeamRequest = {
+    confirmPassword: '',
     CVRNumber: 0,
     VATNumber: '',
     companyName: '',
@@ -45,17 +26,14 @@ export class RegisterComponent implements OnInit {
     zipCode: 0,
     country: '',
     phoneNumber: '',
-    email: this.companyEmail,
     website: '',
-    ownerEmail: this.userEmail
+    companyEmail: ''
   };
 
 
   step1: boolean = true;
   step2: boolean = false;
   step3: boolean = false;
-  openStep2: boolean = false;
-  openStep3: boolean = false;
 
 
   constructor(private router: Router, private authService: AuthService,
@@ -69,29 +47,23 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    this.registerRequest.email = this.userEmail;
-    this.registerRequest.password = this.password;
-    this.registerRequest.confirmPassword = this.confirmPassword;
-    this.authService.registerUser(this.registerRequest).subscribe((data) => {
-      this.showSuccessMessage = data.message;
-      this.businessTeamRequest.CVRNumber = this.CVRNumber;
-      this.businessTeamRequest.VATNumber = this.VATNumber;
-      this.businessTeamRequest.companyName = this.companyName;
-      this.businessTeamRequest.address = this.address;
-      this.businessTeamRequest.city = this.city;
-      this.businessTeamRequest.zipCode = this.zipCode;
-      this.businessTeamRequest.country = this.country;
-      this.businessTeamRequest.phoneNumber = this.phoneNumber;
-      this.businessTeamRequest.email = this.companyEmail;
-      this.businessTeamRequest.website = this.website;
-      this.businessTeamRequest.ownerEmail = this.userEmail;
-      this.teamService.createBusinessTeam(this.businessTeamRequest).subscribe((data) => {
+    const registerRequest: RegisterRequest = {
+      email: this.formData.userEmail,
+      password: this.formData.password,
+      confirmPassword: this.formData.confirmPassword
+    };
+    const businessTeamRequest: BusinessTeamRequest = {
+      ...this.formData,
+      ownerEmail: this.formData.userEmail,
+      email: this.formData.companyEmail
+    };
+
+    this.authService.registerUser(registerRequest).subscribe(() => {
+      this.teamService.createBusinessTeam(businessTeamRequest).subscribe(() => {
         this.toast.success("You have successfully registered.");
         this.navigateToLogin();
-      }, (error) => {
-        this.toast.error("Something went wrong.");
-      });
-    });
+      }, () => this.toast.error("Error creating business team."));
+    }, () => this.toast.error("Error registering user."));
   }
 
   isValidEmail(email: string): boolean {
@@ -114,22 +86,22 @@ export class RegisterComponent implements OnInit {
   }
 
   goToStep2() {
-    if (!this.isValidEmail(this.userEmail)) {
+    if (!this.isValidEmail(this.formData.userEmail)) {
       this.toast.error("Invalid email address.");
       return;
     }
 
-    if (this.password !== this.confirmPassword) {
+    if (this.formData.password !== this.formData.confirmPassword) {
       this.toast.error("Passwords do not match.");
       return;
     }
 
-    if (!this.isValidPassword(this.password)) {
+    if (!this.isValidPassword(this.formData.password)) {
       this.toast.error("Password does not meet criteria: Minimum 8 characters, at least one uppercase letter, one lowercase letter, and one number.");
       return;
     }
 
-    if (this.password !== this.confirmPassword) {
+    if (this.formData.password !== this.formData.confirmPassword) {
       this.toast.error("Passwords do not match.");
       return;
     }
@@ -137,35 +109,28 @@ export class RegisterComponent implements OnInit {
     this.step1 = false;
     this.step2 = true;
     this.step3 = false;
-    this.openStep2 = true;
-    this.openStep3 = false;
   }
 
   goToStep3() {
-    if (!this.isValidCVR(this.CVRNumber)) {
+    if (!this.isValidCVR(this.formData.CVRNumber)) {
       this.toast.error("Invalid CVR number. It should be up to 8 digits long.");
       return;
     }
 
-    if (!this.isValidVAT(this.VATNumber)) {
+    if (!this.isValidVAT(this.formData.VATNumber)) {
       this.toast.error("Invalid VAT number. Format should be DK followed by 8 digits.");
       return;
     }
 
-    // Proceed to Step 3
     this.step1 = false;
     this.step2 = false;
     this.step3 = true;
-    this.openStep2 = false;
-    this.openStep3 = true;
   }
 
   goToStep1() {
     this.step1 = true;
     this.step2 = false;
     this.step3 = false;
-    this.openStep2 = false;
-    this.openStep3 = false;
   }
 
 }
